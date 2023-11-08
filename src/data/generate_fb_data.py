ced23ffdf62ce2817aa6a6caa266ce83dcccb3a2
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2023-11-08 12:19:11 +0100

    change default start data

--- src/data/generate_fb_data.py
+++ src/data/generate_fb_data.py
@@ -2,8 +2,13 @@ FRAMEBUFFER_SIZE = 800 * 480
 FRAMEBUFFER_WIDTH = 800
 FRAMEBUFFER_HEIGHT = 480
 
-with open("fb_data.data", "w+") as f:
+with open("fb_data.coe", "w+") as f:
+    f.write("memory_initialization_radix=2;\n")
+    f.write("memory_initialization_vector=\n")
     for i in range(int(FRAMEBUFFER_SIZE / 2)):
-        f.write(bin(0)[2:].zfill(4) + "\n")
+        f.write(bin(0)[2:].zfill(4) + ",\n")
     for i in range(int(FRAMEBUFFER_SIZE / 2)):
-        f.write(bin(1)[2:].zfill(4) + "\n")
+        if (i == FRAMEBUFFER_SIZE / 2 - 1):
+            f.write(bin(1)[2:].zfill(4) + ";")
+        else:
+            f.write(bin(1)[2:].zfill(4) + ",\n")
