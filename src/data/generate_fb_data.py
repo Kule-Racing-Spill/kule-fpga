d299bec6df62a841ea5fd3ffd02ca8fc5d3eb6d8
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2023-11-14 15:28:53 +0100

    update data

--- src/data/generate_fb_data.py
+++ src/data/generate_fb_data.py
@@ -5,10 +5,10 @@ FRAMEBUFFER_HEIGHT = 480
 with open("fb_data.coe", "w+") as f:
     f.write("memory_initialization_radix=2;\n")
     f.write("memory_initialization_vector=\n")
-    for i in range(int(FRAMEBUFFER_SIZE / 2)):
-        f.write(bin(0)[2:].zfill(4) + ",\n")
-    for i in range(int(FRAMEBUFFER_SIZE / 2)):
-        if (i == FRAMEBUFFER_SIZE / 2 - 1):
-            f.write(bin(1)[2:].zfill(4) + ";")
+    for i in range(int(FRAMEBUFFER_SIZE / 4)):
+        f.write(bin(5)[2:].zfill(4) + ",\n")
+    for i in range(3 * int(FRAMEBUFFER_SIZE / 4)):
+        if (i == 3 * (FRAMEBUFFER_SIZE / 2) - 1):
+            f.write(bin(11)[2:].zfill(4) + ";")
         else:
-            f.write(bin(1)[2:].zfill(4) + ",\n")
+            f.write(bin(11)[2:].zfill(4) + ",\n")
